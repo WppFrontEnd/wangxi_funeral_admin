8a9188bb6993db1002fca836d290e79ca34143b4
WppFrontEnd <wpphomeemail@163.com>
2022-03-12 19:48:47 +0800

    初始化项目

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login-wrape">
+    <div class="bg"><img src="@/assets/images/background.png" />></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login-wrape {
+  .bg {
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
